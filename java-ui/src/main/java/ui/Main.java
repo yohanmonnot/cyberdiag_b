311b770a7cc5f9c9d4dc920b684a742ac2108ffe
Cyforce <celian.touzeau@outlook.com>
2025-10-10 17:41:09 +0200

    add sleep time

--- java-ui/src/main/java/ui/Main.java
+++ java-ui/src/main/java/ui/Main.java
@@ -3,5 +3,10 @@ package ui;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Java UI!");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
