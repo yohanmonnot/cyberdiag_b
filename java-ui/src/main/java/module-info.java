4ddd80286c4652a67a735febf05a5c867cd2a213
Celian Touzeau <touzeau.e2400797@etud.univ-ubs.fr>
2025-12-18 00:20:54 +0100

    ajout ModuleExecutor

--- java-ui/src/main/java/module-info.java
+++ java-ui/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.cyclonicforce.fr.java.ui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
     // OUVRE le package de la CLASSE MAIN (si elle lance le FXML)
     opens edu.cyclonicforce.fr.ui to javafx.fxml;
@@ -8,6 +9,8 @@ module edu.cyclonicforce.fr.java.ui {
     // NOUVEAU : OUVRE le package du CONTROLLER
     // Le FXMLoader doit pouvoir accéder à ce package
     opens edu.cyclonicforce.fr.ui.controller to javafx.fxml;
+
+    opens edu.cyclonicforce.fr.ui.lib.bashExecutor to com.google.gson;
 
     // EXPORTE le package de la classe Main pour qu'il soit accessible depuis le JAR
     exports edu.cyclonicforce.fr.ui;
