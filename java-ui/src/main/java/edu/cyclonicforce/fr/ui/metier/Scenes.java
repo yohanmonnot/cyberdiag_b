42256f7ca826182020d2185fbf200f154dddcf0a
Celian <touzeau.e2400797@etud.univ-ubs.fr>
2026-01-01 13:07:20 +0100

    JE SUIS UN AVION

---
+++ java-ui/src/main/java/edu/cyclonicforce/fr/ui/metier/Scenes.java
@@ -0,0 +1,11 @@
+package edu.cyclonicforce.fr.ui.metier;
+
+public enum Scenes {
+    ACCEUIL,
+    DASHBOARD,
+    DASHBOARD_MODULES,
+    DASHBOARD_DIAGS,
+    DASHBOARD_SETTINGS,
+    DASHBOARD_REPORTS,
+    DASHBOARD_HELP
+}
--- java-ui/src/main/java/edu/cyclonicforce/fr/ui/view/AccueilLoader.java
+++ java-ui/src/main/java/edu/cyclonicforce/fr/ui/view/AccueilLoader.java
@@ -4,7 +4,7 @@ public class AccueilLoader implements ViewLoader {
     @Override
     public javafx.scene.Scene load() {
         try {
-            javafx.fxml.FXMLLoader fxmlLoader = new javafx.fxml.FXMLLoader(getClass().getResource("/edu/cyclonicforce/fr/ui/fxml/Acceuil.fxml"));
+            javafx.fxml.FXMLLoader fxmlLoader = new javafx.fxml.FXMLLoader(getClass().getResource("/edu/cyclonicforce/fr/ui/fxml/Accueil.fxml"));
             return new javafx.scene.Scene(fxmlLoader.load());
         } catch (java.io.IOException e) {
             e.printStackTrace();
