93b2f2a046a036ec21403fe5bab65ad6add409f9
Touzeau Celian <touzeau.e2400797@etud.univ-ubs.fr>
2026-01-10 13:10:31 +0100

    Revert "Merge branch 'dev' into 'main'"
    This reverts merge request !36

--- java-ui/src/main/java/edu/cyclonicforce/fr/ui/metier/ScanType.java
+++
@@ -1,6 +0,0 @@
-package edu.cyclonicforce.fr.ui.metier;
-
-public enum ScanType {
-    MODULE,
-    DIAGNOSTIC
-}
---
+++ java-ui/src/main/java/ui/Controllers/ExampleController.java
@@ -0,0 +1,7 @@
+package ui.Controllers;
+
+public class ExampleController {
+    public void displayMessage() {
+        System.out.println("This is an example controller.");
+    }
+}
