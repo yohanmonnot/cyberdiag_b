ab1b39133eac85d644658b011a354b8cbed6b208
Celian Touzeau <touzeau.e2400797@etud.univ-ubs.fr>
2026-01-21 13:15:31 +0100

    ajout du lien entre java et bash pour les rapports

--- java-ui/src/main/java/edu/cyclonicforce/fr/ui/controller/DashboardReportListController.java
+++ java-ui/src/main/java/edu/cyclonicforce/fr/ui/controller/DashboardReportListController.java
@@ -1,5 +1,6 @@
 package edu.cyclonicforce.fr.ui.controller;
 
+import edu.cyclonicforce.fr.ui.lib.bashExecutor.ReportExec;
 import edu.cyclonicforce.fr.ui.metier.DiagnosticReport;
 import edu.cyclonicforce.fr.ui.metier.DiagnosticReportModule;
 import edu.cyclonicforce.fr.ui.metier.DiagnosticReportModule;
@@ -19,6 +20,13 @@ public class DashboardReportListController implements DasboardController {
     private VBox rootVBox;
 
     private AppController appController;
+    private List<DiagnosticReport> diagnosticReportList;
+    private List<DashboardReportListElementController> dashboardReportListElementControllers;
+
+    public DashboardReportListController() {
+        this.diagnosticReportList = new ArrayList<>();
+        this.dashboardReportListElementControllers = new ArrayList<>();
+    }
 
     @Override
     public void setAppController(AppController appController) {
@@ -26,84 +34,39 @@ public class DashboardReportListController implements DasboardController {
         refresh();
     }
 
-    @FXML
-    public void initialize(){
-        refresh();
-    }
-
     private void refresh(){
         reportBox.getChildren().clear();
+        refreshReportList();
 
+        for (DiagnosticReport report : diagnosticReportList) {
+            addReportElement(report);
+        }
+    }
+
+    private void refreshReportList(){
+        this.diagnosticReportList.clear();
+
+        ReportExec reportExec = new ReportExec();
+
+        this.diagnosticReportList.addAll(reportExec.listReports(null, null, null));
+    }
+
+    private void addReportElement(DiagnosticReport report) {
         ViewLoader<DashboardReportListElementController> loader = new ViewLoader<>();
 
-        // --- MAP POUR RAPPORT 1 (Note finale : 1) ---
-        Map<String, List<DiagnosticReportModule>> map1 = new HashMap<>();
-        List<DiagnosticReportModule> list1 = new ArrayList<>();
-        // Un module qui échoue presque tout (Score 1)
-        list1.add(new DiagnosticReportModule("diskCheck", 0, "", 1, "Remplacer le disque dur immédiatement."));
-        map1.put("Hardware", list1);
+        loader.load("/edu/cyclonicforce/fr/ui/fxml/DashboardReportListElement.fxml", appController);
 
+        DashboardReportListElementController controller = loader.getController();
 
-        // --- MAP POUR RAPPORT 2 (Note finale : 2) ---
-        Map<String, List<DiagnosticReportModule>> map2 = new HashMap<>();
-        List<DiagnosticReportModule> list2 = new ArrayList<>();
-        // Score moyen de 2
-        list2.add(new DiagnosticReportModule("HardCheck", 0, "", 2, "Nettoyer les ventilateurs."));
-        map2.put("System", list2);
-
-
-        // --- MAP POUR RAPPORT 3 (Note finale : 3) ---
-        Map<String, List<DiagnosticReportModule>> map3 = new HashMap<>();
-        // Catégorie 1 : Réseau (Score 3)
-        List<DiagnosticReportModule> list3a = new ArrayList<>();
-        list3a.add(new DiagnosticReportModule("NetChack", 0, "", 3, "Vérifier le câble Ethernet."));
-        map3.put("Network", list3a);
-        // Catégorie 2 : Sécurité (Score 3)
-        List<DiagnosticReportModule> list3b = new ArrayList<>();
-        list3b.add(new DiagnosticReportModule("MalwareCheck", 0, "", 3, "Mettre à jour l'antivirus."));
-        map3.put("Security", list3b);
-
-
-        // --- MAP POUR RAPPORT 4 (Note finale : 4) ---
-        Map<String, List<DiagnosticReportModule>> map4 = new HashMap<>();
-        List<DiagnosticReportModule> list4 = new ArrayList<>();
-        // Score solide de 4
-        list4.add(new DiagnosticReportModule("processCheck", 0, "", 4, "Optimiser les processus de démarrage."));
-        map4.put("Performance", list4);
-
-
-        // --- MAP POUR RAPPORT 5 (Note finale : 5) ---
-        Map<String, List<DiagnosticReportModule>> map5 = new HashMap<>();
-        List<DiagnosticReportModule> list5 = new ArrayList<>();
-        // Score parfait (5)
-        list5.add(new DiagnosticReportModule("sysCheck", 0, "", 5, "Système en parfait état."));
-        list5.add(new DiagnosticReportModule("uiiiiiiiiiiiiiii", 0, "", 5, "Aucune action requise."));
-        map5.put("Optimization", list5);
-
-
-        // 2. Création du tableau de rapports
-        DiagnosticReport[] reports = new DiagnosticReport[]{
-                new DiagnosticReport("Rapport Critique", "2024-01-15 09:30:00", 0, map1),
-                new DiagnosticReport("Rapport Faible",   "2024-02-20 14:15:00", 0, map2),
-                new DiagnosticReport("Rapport Moyen",    "2024-03-10 10:00:00", 0, map3),
-                new DiagnosticReport("Rapport Bon",      "2024-04-05 16:45:00", 0, map4),
-                new DiagnosticReport("Rapport Excellent","2024-05-12 16:45:00", 1, map5),
-        };
-
-        for (DiagnosticReport report : reports) {
-            loader.load("/edu/cyclonicforce/fr/ui/fxml/DashboardReportListElement.fxml", appController);
-
-            DashboardReportListElementController controller = loader.getController();
-
-            controller.setDiagnosticReport(report);
-            if (appController != null) {
-                controller.setAppController(appController);
-            } else {
-                System.err.println("AppController is null in DashboardReportListController");
-            }
-
-            reportBox.getChildren().add(loader.getRoot());
+        controller.setDiagnosticReport(report);
+        if (appController != null) {
+            controller.setAppController(appController);
+        } else {
+            System.err.println("AppController is null in DashboardReportListController");
         }
+        this.dashboardReportListElementControllers.add(controller);
+
+        reportBox.getChildren().add(loader.getRoot());
     }
 
     /**
